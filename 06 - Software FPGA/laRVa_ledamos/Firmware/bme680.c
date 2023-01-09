cca3f34173571577f3d1e54a985174b0146326fb
pvilayl <pablovillacorta29@gmail.com>
2023-01-09 18:40:19 +0100

    Modificado fichero bme680.c

--- 06 - Software FPGA/laRVa_ledamos/Firmware/bme680.c
+++ 06 - Software FPGA/laRVa_ledamos/Firmware/bme680.c
@@ -15,21 +15,34 @@
 #define reset_BME  			0x60
 #define Id_BME  			0x50
 #define Config_BME  		0x75
+
 #define Ctrl_meas_BME  		0x74  // osrs_t [7:5] osrs_p [4:2] mode [1:0]
 #define Ctrl_hum_BME  		0x72  // osrs_h [2:0]
+
 #define Ctrl_gas_1_BME  	0x71
 #define Ctrl_gas_0_BME  	0x70
 
-#define gas_wait9			0x6D
-#define gas_wait8			0x6C
-#define gas_wait7			0x6B
-#define gas_wait6			0x6A
-#define gas_wait5			0x69
-#define gas_wait4			0x68
-#define gas_wait3			0x67
-#define gas_wait2			0x66
-#define gas_wait1			0x65
-#define gas_wait0			0x64
+#define gas_wait0			0x6D
+#define gas_wait1			0x6C
+#define gas_wait2			0x6B
+#define gas_wait3			0x6A
+#define gas_wait4			0x69
+#define gas_wait5			0x68
+#define gas_wait6			0x67
+#define gas_wait7			0x66
+#define gas_wait8			0x65
+#define gas_wait9			0x64
+
+#define res_heat0			0x63
+#define res_heat1			0x62
+#define res_heat2			0x61
+#define res_heat3			0x60
+#define res_heat4			0x5F
+#define res_heat5			0x5E
+#define res_heat6			0x5D
+#define res_heat7			0x5C
+#define res_heat8			0x5B
+#define res_heat9			0x5A
 
 // READ ONLY REGS
 #define gas_r_lsb_BME		0x2B
@@ -56,7 +69,7 @@
 
 // --------------------------------------------------------
 
-uint8_t spixfer (uint8_t d)
+uint8_t spixfer(uint8_t d)
 {
 	SPIDAT=d;
 	while(SPISTA&1);
@@ -68,7 +81,7 @@ uint8_t spixfer (uint8_t d)
 char readBME680(char dir){
 	char dataread=0x00;
 	SPISS=BME680_CS;
-	spixfer (RD|dir);
+	spixfer(RD|dir);
 	dataread = spixfer(0x00);
 	SPISS=0b11;
 	return dataread; //send dummy byte
@@ -78,10 +91,11 @@ char readBME680(char dir){
 
 void writeBME680(char data,char dir){
 	SPISS=BME680_CS;
-	spixfer (dir);
+	spixfer(dir);
 	spixfer(data);
 	SPISS=0b11;
 }
+
 
 void readAllRegs(void) 
 { 
@@ -106,14 +120,35 @@ void readAllRegs(void)
 	//dataread = spixfer(0x00);	//send dummy byte
 }
 
-void startBME680(void){  // Inicialización del sensor BME
-	char dir;
+
+
+// -------------- BME init ---------------------
+void startBME680(void){ 
 	// Set humidity oversampling to 1x (osrs_h = Ctrl_hum_BME[2:0] = 001)
-	writeBME680(0b00000001,Ctrl_hum_BME);
+	writeBME680(0b00000 001,Ctrl_hum_BME);
 	
 	// Set temperature oversampling to 2x (osrs_t = Ctrl_meas_BME[7:5] = 010)
 	// Set pressure oversampling to 16x (osrs_p = Ctrl_meas_BME[4:2] = 101)
-	writeBME680(0b01010100,Ctrl_meas_BME);
+	writeBME680(0b010 101 00,Ctrl_meas_BME);
 	
-	// (...)
+	// Set gas_wait_0 to 0x59 to select 100 ms heat up duration
+	writeBME680(0x59,gas_wait0);
+	
+	// Set the corresponding heater set-point by writing the target heater resistance to res_heat_0  
+	writeBME680(0,res_heat0);
+	
+	// Set nb_conv to 0x0 to select the previously defined heater settings (nb_conv = Ctrl_gas_1_BME[3:0])
+	// Set run_gas_l to 1 to enable gas measurements  (run_gas_l = Ctrl_gas_1_BME[4])
+	writeBME680(0b000 1 0000,Ctrl_gas_1_BME);
+	
+	// Set mode to 0b01 to trigger a single measurement. (mode = Ctrl_meas_BME[1:0])
+	writeBME680(0b010 101 01,Ctrl_meas_BME);
 }
+
+// ------------ Temperature measurement ---------------
+float tempBME680(void){
+	
+}
+
+
+
